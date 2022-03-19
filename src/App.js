fad4ef926445d537a1f1fffe991f2a2bc5033781
Sophie Newell <sophienewellstudio@gmail.com>
2022-03-19 15:59:15 -0500

    adds routes for home page and search page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import "./App.css";
+import React from "react";
+import PlantSearchPage from "./components/PlantSearchPage";
+import HomePage from "./components/HomePage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/search" element={<PlantSearchPage />} />
+          <Route path="*" element={<Navigate to="/home" />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
 
 export default App;
