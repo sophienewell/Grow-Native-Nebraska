1b9f8c46a4462b22137891531b21809d54536a15
Sophie Newell <sophienewellstudio@gmail.com>
2022-03-19 17:05:40 -0500

    finishes filters

--- src/components/PlantSearchPage.jsx
+++ src/components/PlantSearchPage.jsx
@@ -131,13 +131,86 @@ function PlantSearchPage() {
       sun: "Full sun",
       soiltype: "Wet",
     },
+    //Full sun, medium soil
+    {
+      name: "Prairie Phlox",
+      bloomTime: "Spring",
+      color: "Pink",
+      height: "Short",
+      sun: "Full sun",
+      soiltype: "Medium",
+    },
+
+    //Full sun, dry soil
+    {
+      name: "Prairie Pussytoes",
+      bloomTime: "Spring",
+      color: "White",
+      height: "Ground cover",
+      sun: "Full sun",
+      soiltype: "Dry",
+    },
+    //Partial sun, Wet soil
+    {
+      name: "Calico Aster",
+      bloomTime: "Fall",
+      color: "White",
+      height: "Medium",
+      sun: "Partial sun",
+      soiltype: "Wet",
+    },
+    //Partial sun, Medium soil
+    {
+      name: "Wild Geranium",
+      bloomTime: "Spring",
+      color: "Pink",
+      height: "Short",
+      sun: "Partial sun",
+      soiltype: "Medium",
+    },
+    //Partial sun, Dry soil
+    {
+      name: "Columbine",
+      bloomTime: "Spring",
+      color: "Red",
+      height: "Medium",
+      sun: "Full sun",
+      soiltype: "Dry",
+    },
+    //Full shade, Wet soil
+    {
+      name: "Virginia Bluebells",
+      bloomTime: "Spring",
+      color: "Blue",
+      height: "Short",
+      sun: "Full shade",
+      soiltype: "Wet",
+    },
+    //Full shade, medium soil
+    {
+      name: "Hairy Beardtongue",
+      bloomTime: "Spring",
+      color: "White",
+      height: "Medium",
+      sun: "Full shade",
+      soiltype: "Medium",
+    },
+    //Full shade, dry soil
+    {
+      name: "Jacob's Ladder",
+      bloomTime: "Spring",
+      color: "White",
+      height: "Short",
+      sun: "Full shade",
+      soiltype: "dry",
+    },
   ];
 
   const [sunAmount, setSunAmount] = useState("all");
   const [soilType, setSoilType] = useState("all");
   const [bloomTime, setBloomTime] = useState("all");
   const [color, setColor] = useState("all");
-  const [size, setSize] = useState("all");
+  const [height, setHeight] = useState("all");
 
   return (
     <div>
@@ -199,9 +272,9 @@ function PlantSearchPage() {
           <br />
           <label htmlFor="size">Height</label>
           <select
-            id="size"
-            value={size}
-            onChange={(e) => setSize(e.target.value)}
+            id="height"
+            value={height}
+            onChange={(e) => setHeight(e.target.value)}
           >
             <option value="all">All</option>
             <option value="groundCover">Ground cover</option>
@@ -212,6 +285,7 @@ function PlantSearchPage() {
         </div>
       </form>
       {plantList
+        //filter by sun amount
         .filter((val) => {
           if (sunAmount === "fullSun") {
             return val.sun === "Full sun";
@@ -221,6 +295,55 @@ function PlantSearchPage() {
             return val.sun === "Full shade";
           } else return val;
         })
+        //filter by soil type
+        .filter((val) => {
+          if (soilType === "wet") {
+            return val.soiltype === "Wet";
+          } else if (soilType === "medium") {
+            return val.soiltype === "Medium";
+          } else if (soilType === "dry") {
+            return val.soiltype === "Dry";
+          } else return val;
+        })
+        //filter by bloom time
+        .filter((val) => {
+          if (bloomTime === "spring") {
+            return val.bloomTime === "Spring";
+          } else if (bloomTime === "summer") {
+            return val.bloomTime === "Summer";
+          } else if (bloomTime === "fall") {
+            return val.bloomTime === "Fall";
+          } else return val;
+        })
+        //filter by flower color
+        .filter((val) => {
+          if (color === "yellow") {
+            return val.color === "Yellow";
+          } else if (color === "white") {
+            return val.color === "White";
+          } else if (color === "purple") {
+            return val.color === "Purple";
+          } else if (color === "red") {
+            return val.color === "Red";
+          } else if (color === "pink") {
+            return val.color === "Pink";
+          } else if (color === "blue") {
+            return val.color === "Blue";
+          } else return val;
+        })
+        //filter by height
+        .filter((val) => {
+          if (height === "groundCover") {
+            return val.height === "Ground cover";
+          } else if (height === "short") {
+            return val.height === "Short";
+          } else if (height === "medium") {
+            return val.height === "Medium";
+          } else if (height === "tall") {
+            return val.height === "Tall";
+          } else return val;
+        })
+
         .map((val) => {
           console.log(val.name);
         })}
