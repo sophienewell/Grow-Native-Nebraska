0349054028c6c0090cd843b4c887114f64065d5b
Sophie Newell <sophienewellstudio@gmail.com>
2022-03-24 14:55:09 -0500

    adds form for Signup and useAPI hook template

--- src/components/MailingListPage.jsx
+++ src/components/MailingListPage.jsx
@@ -1,3 +1,42 @@
-function MailingListPage() {}
+import React, { useRef, useState, useCallback } from "react";
+
+function MailingListPage() {
+  const emailInput = useRef(null);
+  const { signUp } = useAPI();
+  const [err, setErr] = useState("");
+  const handleSignup = useCallback(async () => {
+    const emailAdd = emailInput.current.value;
+    if (
+      emailAdd.length < 6 ||
+      emailAdd.length > 30 ||
+      !emailAdd.includes("@")
+    ) {
+      return;
+    }
+    const json = await signUp(emailAdd);
+    if (!json.success) {
+      setErr(json.error);
+    } else {
+      return;
+    }
+  });
+  return (
+    <form className="center">
+      <label htmlFor="email">
+        Enter your email here to sign up for our mailing list
+      </label>
+      <br />
+      <input
+        id="email"
+        type="text"
+        ref={emailInput}
+        placeholder="somebody@something.com"
+      />
+      <br />
+      <br />
+      <button onClick={handleSignup}>Add</button>
+    </form>
+  );
+}
 
 export default MailingListPage;
