ef9c21e847c4badf31a6f6919263d17ecf0ed082
Sophie Newell <sophienewellstudio@gmail.com>
2022-03-24 15:51:31 -0500

    adds server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + "/build"));
+app.use(express.json());
+
+app.get("*", (req, res) => {
+  return res.sendFile("/build/index.html", { root: __dirname + "/" });
+});
+
+app.listen(PORT, () => console.log(`Server is running on port" ${PORT}!`));
