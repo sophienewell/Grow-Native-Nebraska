09717aae3e388123dcfcb21ffee57b3c2aa424a4
Sophie Newell <sophienewellstudio@gmail.com>
2022-03-25 12:31:31 -0500

    adds error message for non-email format

--- server/middleware/minmax.middleware.js
+++ server/middleware/minmax.middleware.js
@@ -1,9 +1,14 @@
 function minmax(req, res, next) {
   const { email_address } = req.body;
-  if (!email_address || email_address.length < 6 || email_address.length > 30) {
+  if (
+    !email_address ||
+    email_address.length < 6 ||
+    email_address.length > 30 ||
+    !email_address.includes("@")
+  ) {
     return res.send({
       success: false,
-      error: "Email must be between 6-30 characters",
+      error: "Invalid email address",
       data: null,
     });
   }
